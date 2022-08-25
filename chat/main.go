82f8cf39accde362e9799f3cc6097247344c01ef
Golibbek0414 <ashirovgolibbek2@gmail.com>
2022-08-25 10:29:19 +0500

    online_chat

---
+++ chat/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a, b int
+	fmt.Printf("Ikkita son kiriting: ")
+	fmt.Scan(&a,&b)
+	var c int
+	fmt.Printf("tanlash operatoridan foydalaning")
+	fmt.Scan(&c)
+	switch(c){
+		case 1 {
+			
+		}
+	}
+}
